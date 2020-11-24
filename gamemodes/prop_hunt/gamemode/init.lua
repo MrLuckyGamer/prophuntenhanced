9e7e8040a032fcbd657c39be602f25ffa755be4c
Lucky <43052474+MrLuckyGamer@users.noreply.github.com>
2020-11-24 19:58:34 +1100

    Add files via upload

--- gamemodes/prop_hunt/gamemode/init.lua
+++ gamemodes/prop_hunt/gamemode/init.lua
@@ -12,6 +12,7 @@ AddCSLuaFile("cl_menu.lua")
 AddCSLuaFile("cl_targetid.lua")
 AddCSLuaFile("cl_autotaunt.lua")
 AddCSLuaFile("cl_credits.lua")
+AddCSLuaFile( "cl_scoreboard.lua" )
 
 -- Include the required lua files
 include("sv_networkfunctions.lua")
